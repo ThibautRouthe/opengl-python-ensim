da63ec253da99aaab32932ab2d6d9628142b9ff9
Thibaut ROUTHE <e2104605@ensimlan.univ-lemans.fr>
2023-12-05 11:06:44 +0100

    tp1 3 squares

--- 3squares.py
+++ 3squares.py
@@ -21,18 +21,31 @@ def render_Scene():
     # Set current color to red
     glColor3f(1.0,0.0,0.0)
 
+    glPointSize(5.0)
+    
     # Draw a red square
-    glBegin(GL_POLYGON)
-    glVertex2f(0.20,-0.20)
-    glVertex2f(0.5,-1.0)
-    glVertex2f(0.0,-0.15)
-    glVertex2f(-0.5,-1.0)
-    glVertex2f(-0.20,-0.20)
-    glVertex2f(-1.0,0.15)
-    glVertex2f(-0.15,0.15)
-    glVertex2f(0.0,1.0)
-    glVertex2f(0.15,0.15)
-    glVertex2f(1.0,0.15)
+    glBegin(GL_POINTS)
+    glVertex2f(0.15, 0.15)
+    glEnd()
+    
+    # Set current color to red
+    glColor3f(0.0,1.0,0.0)
+
+    glPointSize(10.0)
+    
+    # Draw a red square
+    glBegin(GL_POINTS)
+    glVertex2f(-0.3, 0.15)
+    glEnd()
+    
+    # Set current color to red
+    glColor3f(0.0,0.0,1.0)
+
+    glPointSize(20.0)
+    
+    # Draw a red square
+    glBegin(GL_POINTS)
+    glVertex2f(-0.2, -0.2)
     glEnd()
 
 # Initialize GLUT
--- image_synthesis.py
+++ image_synthesis.py
@@ -8,3 +8,50 @@ Created on Tue Dec  5 09:38:04 2023
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
+
+poly = [
+        [0.0,1.0],
+        [1.0,1.0],
+        [1.0,0.0],
+        [0.0,0.0],
+        ]
+
+# Disply callback function
+def display():
+    # Reset background
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    glColor3f(1.0,0.0,0.0)
+    # Render scene
+    drawPoly()
+
+    # Swap buffers
+    glutSwapBuffers()
+    
+def drawPoly():
+    glBegin(GL_POLYGON)
+    for vertex in poly:
+        glVertex2f(vertex[0],vertex[1])
+    glEnd()
+
+
+# Initialize GLUT
+glutInit()
+
+# Initialize the window with double buffering and RGB colors
+glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
+
+# Set the window size to 500x500 pixels
+glutInitWindowSize(500, 500)
+
+# Create the window and give it a title
+glutCreateWindow("My First OpenGL Window")
+
+# Set the initial window position to (50, 50)
+glutInitWindowPosition(50, 50)
+
+# Define callbacks
+glutDisplayFunc(display)
+
+# Begin event loop
+glutMainLoop()
